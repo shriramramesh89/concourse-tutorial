228fd7823b1b19ed69deddbc4c030c3547da1c4f
Michael Brodhead <mkb@orthogonal.org>
2016-12-09 13:49:59 -0800

    Working on deploying Concourse from Concourse. Good times.

--- ci/lib/shared-functions.sh
+++ ci/lib/shared-functions.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
 
 function ensure-rvm {
+  announce-task "Making sure RVM is set up..."
   set +ux
   if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
-    source "$HOME/.rvm/scripts/rvm"
+    run-cmd source "$HOME/.rvm/scripts/rvm"
   elif [[ -s "/usr/local/rvm/scripts/rvm" ]] ; then
-    source "/usr/local/rvm/scripts/rvm"
+    run-cmd source "/usr/local/rvm/scripts/rvm"
   else
-    printf "ERROR: An RVM installation was not found.\n"
+    echo -e "${BRed}ERROR: ${Red}An RVM installation was not found.\n${RCol}"
   fi
-  set -ux
+  set -u
 }
 
 function bosh-login {
+  announce-task "Logging-in to BOSH director..."
   which bosh
   bosh version
 
   set +x
   bosh -t ${bosh_director} login ${bosh_username} ${bosh_password}
-  set -x
-  bosh target ${bosh_director}
-  bosh status
+  run-cmd bosh target ${bosh_director}
+  run-cmd bosh status
+}
+
+function deploy-concourse {
+  announce-task "Starting deployment..."
+  run-cmd bosh deployment ci/manifest.yml
+  run-cmd bosh -n deploy
+}
+
+function check-concourse {
+  announce-task "Making sure Concourse is up..."
+  run-cmd fly target tutorial http://10.244.8.2:8080
+  fly login -t tutorial --username=concourse --password=concourse
+  run-cmd fly -t tutorial pipelines
 }
